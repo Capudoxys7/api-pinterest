5f740095480892ca6dd6f40290ff8ca5c3f5f56f
Capudoxys7 <capudocleiton7@gmail.com>
2024-09-15 10:11:39 -0300

    Update app.py

--- app.py
+++ app.py
@@ -11,26 +11,13 @@ app = Flask(__name__)
 # Função para buscar o vídeo no YouTube usando cookies
 def search_video(query):
     ydl_opts = {
-        'quiet': True,
-        'format': 'bestaudio/best',
-        'noplaylist': True,
-        'extract-audio': True,
-        'audio-format': 'mp3',
-        'http_headers': {
-            'Cookie': ('VISITOR_PRIVACY_METADATA=CgJCUhIEGgAgJg%3D%3D; __Secure-3PSID=g.a000oAh8TI1n49_Ux3Xu-EC6OlkxjHAXM6x5miSzqwNrlPyElpQL1FzkiQamrdp6Ao-S4wi-jgACgYKAXYSARMSFQHGX2Mi3-ioeMxM6g-eQB45iH09uBoVAUF8yKqEOkk5-7xuugUSgqtZkEQy0076; '
-                        'GPS=1; SIDCC=AKEyXzV9rY--p45z6CSNtvGiWYgRv1tmZ0xk5ku9LAwLTk6TIg92JaoPgBTKpof4UyKRI10h; YSC=NKJ1_jqNy2s; '
-                        'SID=g.a000oAh8TI1n49_Ux3Xu-EC6OlkxjHAXM6x5miSzqwNrlPyElpQLu-qY3QLRBJ1B02NyeCU05QACgYKAdoSARMSFQHGX2MisxQByu83nxiI8L2rUKX-XBoVAUF8yKpTvReDAG2bpSfUoB3oSW0c0076; '
-                        '__Secure-1PSIDTS=sidts-CjEBUFGoh1apV30CyRjKLmKqhixkwGFzC3kI1xjJsPqV06kkxxxuilXeUtnJlWUzZ3_GEAA; '
-                        'SAPISID=4CNo5uWwFSZG9b0G/AXzPiQ2L6a2WeVeML; __Secure-1PSIDCC=AKEyXzUympzdridXk38ln7MzIm4DTGYzCRRS3_yY_ZH3yHIZYaI7SbIEzXrE4_5YyinUPpKYnw; SSID=A1_LFRCq0n5oSEBBr; '
-                        '__Secure-1PAPISID=4CNo5uWwFSZG9b0G/AXzPiQ2L6a2WeVeML; __Secure-1PSID=g.a000oAh8TI1n49_Ux3Xu-EC6OlkxjHAXM6x5miSzqwNrlPyElpQLF9lbH0035uB1EzYg0VfPHgACgYKAUcSARMSFQHGX2Mi2g4ovCqvp54IpDQMSqD9BRoVAUF8yKp20lbQ6HdFnWbqIeyeKMOe0076; '
-                        '__Secure-3PAPISID=4CNo5uWwFSZG9b0G/AXzPiQ2L6a2WeVeML; __Secure-3PSIDCC=AKEyXzUgMP9ByGOSkkpHRFGBaXfHtRTS7_liqfcOXj1pr6ch5kPWlxANCGFhAQ6qc2fLWkK5; '
-                        '__Secure-3PSIDTS=sidts-CjEBUFGoh1apV30CyRjKLmKqhixkwGFzC3kI1xjJsPqV06kkxxxuilXeUtnJlWUzZ3_GEAA; '
-                        'APISID=DiI70Ovr8Yrz6M30/AxM5EK3i_UXqOsJnU; HSID=A4TGwqBZkE8cI4gFU; '
-                        'LOGIN_INFO=AFmmF2swRQIgPGo0mCrlgbAjl1o2BRkL9g19Jzeb_pDpe9qPR7Mx2iUCIQCa8uLKzWF4S0SfQRmEOWSLnvJ2ESqlAMDcJGhAWsYMjw:QUQ3MjNmeVczS1RVb19UQUhxZzBHbnhEWUVyQ204TGVySldjSHdveWJEdmhIeGVUOGdjLW1YSGJqOG5oNDJDOFN1VXBUVEpVQmxmX0FDaUtITjVkMGdEbXVTbDNXX2ZMbGNNb1R6dHVZTm44azFXU2RqR3RhemtqOHJPZTNoamJTR25vdmtLaTROenRQNG5seHlPRW1kNk8tUXdZV1VjQndB; '
-                        'PREF=f6=40000000&tz=America.Fortaleza; VISITOR_INFO1_LIVE=p-s6PkkBDJQ')
-        }
+    'quiet': True,
+    'format': 'bestaudio/best',
+    'noplaylist': True,
+    'extract-audio': True,
+    'audio-format': 'mp3',
+    'cookiefile': 'cookies.txt'  # Arquivo de cookies no formato Netscape
     }
-
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         result = ydl.extract_info(f"ytsearch:{query}", download=False)
         if result:
